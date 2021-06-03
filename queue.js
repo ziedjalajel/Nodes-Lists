edca54ed7923fcb8cd7add2bd1f91f370f6fa036
ziedjalajel <zied.jalajel1993@gmail.com>
2021-06-03 11:16:24 +0300

    Queue

--- queue.js
+++ queue.js
@@ -9,23 +9,62 @@ class Node{
       constructor(limit=10, front=null, back=null){
           this.limit =limit
           this.size=0
-          let persons
-          let time = persons*.5
+          this.front
+          this.back
+          this.waitingTime = 0
       }
       peek = ()=> {
         if(this.isEmpty()) console.log("Empty row")
-        else return this.front.data;
+        else return this.front.data;}
       isEmpty=()=>this.size ===0
       isFull=()=>this.size===this.limit
-    enqueue = (groupSize)=>{
-      if(this.isFull())console.log("wait till next round");
-      else const newNode = new node(groupSize);
-      if(isEmpty()){
-        this.front = newNode
-        this.back = newNode
-      }
-    } 
+      addNode=(groupSize)=>{
+        let node = new Node(groupSize)
+        if(this.isEmpty())
+        this.front = node
+        else this.back.next = node
+        this.back=node
+        this.size+=1
+        this.waitingTime+=groupSize*0.5
+      };
+    
+    enqueue=(groupSize)=>{
+      if(this.isFull())
+      console.log("wait till next round");
+      else{ let numberOfPeople= groupSize
+        while(numberOfPeople>12){
+          this.addNode(12)
+          numberOfPeople-=12
+        }
+        this.addNode(numberOfPeople)
+      
+        }
     }
-  }
-  const gameQueue = new queue(12)
-  gameQueue.enqueue()
+    
+    dequeue=()=>{
+      if(this.isEmpty())
+      console.log("no waiting time")
+      else if(this.size===1){
+        let removedNode=this.back
+        this.back=null
+        this.front = null
+        this.waitingTime=0
+        this.size=0
+      }
+      else {
+        let removedNode = this.front
+        this.front = removedNode.next
+        this.size--
+        this.waitingTime-=removedNode.groupSize*0.5
+        return removedNode.groupSize
+      }
+    }
+  } 
+  const ride = new Queue()
+  console.log(ride.waitingTime)
+  ride.enqueue(4)
+  ride.enqueue(8)
+  ride.enqueue(20)
+  console.log(ride.waitingTime)
+  ride.dequeue()
+  console.log(ride.waitingTime)
